a70c1c0ae03b4217ea1b7005f7668a1ac2242644
Itux <118737780+Itux11@users.noreply.github.com>
2024-10-09 08:12:31 -0300

    Update login_process.php

--- MYPC-PROJECT/PaginaLogin/login_process.php
+++ MYPC-PROJECT/PaginaLogin/login_process.php
@@ -1,29 +1,28 @@
 <?php
 session_start();
 
-// Conexão com o banco de dados MYPC_DB
-$conn = new mysqli("localhost", "root", "", "MYPC_DB");
+$conn = new mysqli("localhost", "root", "", "database_mypc");
 
 if ($conn->connect_error) {
     die("Erro de conexão: " . $conn->connect_error);
 }
 
-// Verifica se o formulário foi enviado
+
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $email = $_POST['email'] ?? '';
-    $password = $_POST['password'] ?? '';
+    $senha = $_POST['senha'] ?? '';
     
-    // Prepara a consulta SQL
-    $stmt = $conn->prepare("SELECT * FROM users WHERE email = ?");
+
+    $stmt = $conn->prepare("SELECT * FROM usuarios WHERE email = ?");
     $stmt->bind_param("s", $email);
     $stmt->execute();
     $result = $stmt->get_result();
 
     if ($result->num_rows > 0) {
         $user = $result->fetch_assoc();
-        if (password_verify($password, $user['password'])) {
-            $_SESSION['user_name'] = $user['name'];
-            header("Location: PAG_HOME.php");
+        if (password_verify($senha, $user['senha'])) {
+            $_SESSION['user_name'] = $user['nome'];
+            header("Location: /MYPC-PROJECT/PAG_HOME.php");
             exit();
         } else {
             echo "Senha incorreta!";
